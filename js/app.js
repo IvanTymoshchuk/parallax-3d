1382593a0c4aa4f71ff043863aeb78f682fdc00f
Ivan Tymoshchuk <timohin2231@gmail.com>
2024-01-04 17:36:19 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 document.addEventListener("mousemove", (e) => {
   Object.assign(document.documentElement, {
     style: `
-    --move-x:${e.clientX};
-    --move-y:${e.clientY};
+    --move-x:${(e.clientX - window.innerWidth / 2) * 0.01}deg;
+    --move-y:${(e.clientY - window.innerHeight / 2) * 0.01}deg;
     `,
   });
 });
